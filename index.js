fc0ee9217c4a3db5193c75cec4282d85fe4d25eb
jerson100 <juamkoo@gmail.com>
2021-09-03 13:29:06 -0500

    Pequeños cambios para poder conectarnos a la base de datos del servicio mongo atlas

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-require("dotenv").config();
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
 
 const express = require("express");
 const cors = require("cors");
 const { connectionMongoDB } = require("./api/configs/database");
 
 const app = express();
+
+app.set("PORT", process.env.PORT);
+app.set("APIVERSION", process.env.APIVERSION);
+app.set("NODE_ENV", process.env.NODE_ENV);
 
 //realizamos nuestra conexión a la base de datos.
 connectionMongoDB();
@@ -15,19 +21,26 @@ app.use(express.json());
 
 //first route method
 app.use(
-  `/api/${process.env.APIVERSION}/tasks`,
+  `/api/${app.get("APIVERSION")}/tasks`,
   require("./api/routes/task.router")
 );
 
 //ponemos a la escucha el server para poder aceptar las peticiones de los usuarios.
-app.listen(process.env.PORT, () => {
-  console.log(`Se está escuchando en el puerto: ${process.env.PORT}`);
+app.listen(app.get("PORT"), () => {
+  console.log(`Se está escuchando en el puerto: ${app.get("PORT")}`);
 });
 
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
-  res.send({
-    message: error.message,
-    stack: error.status ? undefined : error.stack,
-  });
+  if (app.get("NODE_ENV") === "development") {
+    res.send({
+      message: error.message,
+      stack: error.status ? undefined : error.stack,
+    });
+  } else {
+    res.send({
+      message:
+        "Ocurrió un error en el servidor, inténtelo más tarde o póngase en contacto con el administrador",
+    });
+  }
 });
